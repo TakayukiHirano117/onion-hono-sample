07c72aeec3d9b8d18d990e21fcfe4f984f1c957a
hirano-ta <hirano-ta@ibjapan.jp>
2026-06-19 13:22:29 +0900

    色々修正

--- src/Domain/Member/member.ts
+++ src/Domain/Member/member.ts
@@ -1,23 +1,29 @@
-import { UUID } from '../shared/vo/uuid'
-import { Name } from './vo/name'
-import { Email } from '../shared/vo/email'
+import { UUID } from "../shared/vo/uuid";
+import { Name } from "./vo/name";
+import { Email } from "../shared/vo/email";
 
 export class Member {
   constructor(
     private readonly _id: UUID,
     private readonly _name: Name,
-    private readonly _email: Email
+    private readonly _email: Email,
   ) {}
 
+  static create(id: UUID, name: string, email: string): Member {
+    return new Member(id, new Name(name), new Email(email));
+  }
+
+  // ドメインロジック追加
+
   get id(): UUID {
-    return this._id
+    return this._id;
   }
 
   get name(): Name {
-    return this._name
+    return this._name;
   }
 
   get email(): Email {
-    return this._email
+    return this._email;
   }
 }
